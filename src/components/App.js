694e79f2c1453d518396d5450197949c32c50c8e
Victor LEDEZ <ledezvictor@gmail.com>
2021-03-11 10:50:27 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
+import history from '../history';
+
+export default function App() {
+  return (
+    <div className='ui container'>
+      <Router history={history}>
+        <div>
+          <Header />
+          <Switch>
+            <Route path='/' exact={true} component={StreamList} />
+            <Route path='/streams/new' exact={true} component={StreamCreate} />
+            <Route
+              path='/streams/edit/:id'
+              exact={true}
+              component={StreamEdit}
+            />
+            <Route
+              path='/streams/delete/:id'
+              exact={true}
+              component={StreamDelete}
+            />
+            <Route
+              path='/streams/show/:id'
+              exact={true}
+              component={StreamShow}
+            />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+}
